2d28e1066d807a171ed90133391b58cfdfdea355
Leonid Beder <leonid@jammer.fm>
2014-02-10 00:29:55 +0200

    Added module.

--- app/controllers/health_monitor/health_controller.rb
+++ app/controllers/health_monitor/health_controller.rb
@@ -1,22 +1,24 @@
-class HealthController < ActionController::Base
-  layout nil
+module HealthMonitor
+  class HealthController < ActionController::Base
+    layout nil
 
-   # GET /health/check
-  def check
-    HealthMonitor.check!
+     # GET /health/check
+    def check
+      HealthMonitor.check!
 
-    render text: "Healh check has passed: #{Time.now.to_s(:db)}\n"
-  rescue Exception => e
-    render text: "Healh check has failed: #{Time.now.to_s(:db)}, error: #{e.message}\n",
-      :status => :service_unavailable
-  end
-
-  private
-  def process_with_silence(*args)
-    logger.quietly do
-      process_without_silence(*args)
+      render text: "Healh check has passed: #{Time.now.to_s(:db)}\n"
+    rescue Exception => e
+      render text: "Healh check has failed: #{Time.now.to_s(:db)}, error: #{e.message}\n",
+        :status => :service_unavailable
     end
-  end
 
-  alias_method_chain :process, :silence
+    private
+    def process_with_silence(*args)
+      logger.quietly do
+        process_without_silence(*args)
+      end
+    end
+
+    alias_method_chain :process, :silence
+  end
 end
