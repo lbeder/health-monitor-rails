7b0613933b494f1667adedd621816aa2ce792d41
Leonid Beder <leonid@jammer.fm>
2015-05-31 11:29:55 +0300

    Restored file name.

--- lib/health-monitor-rails.rb
+++ lib/health-monitor-rails.rb
@@ -1,4 +1,8 @@
+# rubocop:disable Style/FileName
+
 require 'health_monitor/version'
 require 'health_monitor/engine'
 require 'health_monitor/configuration'
 require 'health_monitor/monitor'
+
+# rubocop:enable Style/FileName
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require 'health_monitor_rails'
+require 'health-monitor-rails'
 
 module Dummy
   class Application < Rails::Application
