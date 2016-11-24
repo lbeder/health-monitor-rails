5bbc78a7d2aaa8b3eaf51a896a24ca4d8f53792c
Leonid Beder <leonid@visualead.com>
2016-11-24 22:08:50 +0200

    Fixed specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ require 'coveralls'
 Coveralls.wear!
 
 Spork.prefork do
-  ENV['RAILS_ENV'] ||= 'test'
-
   require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
   require 'rspec/rails'
