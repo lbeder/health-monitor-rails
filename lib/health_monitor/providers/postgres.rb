1ac4565b64eaba1c3f7b6e37065e3eaf3fabb55d
hibishnup <bishnu.pandey@cloudfactory.com>
2017-11-15 09:27:30 +0545

    Add postgres library that checks connection status

---
+++ lib/health_monitor/providers/postgres.rb
@@ -0,0 +1,40 @@
+require 'health_monitor/providers/base'
+# require 'mongo'
+# Mongo::Logger.logger.level = ::Logger::DEBUG
+
+module HealthMonitor
+  module Providers
+    class PostgresException < StandardError; end
+
+    class Postgres < Base
+      class Configuration
+        DEFAULT_CONFIG = {}
+
+        attr_accessor :params
+
+        def initialize
+          @params = DEFAULT_CONFIG
+        end
+      end
+
+      class << self
+        private
+
+        def configuration_class
+          ::HealthMonitor::Providers::Postgres::Configuration
+        end
+      end
+
+      def check!
+        begin
+          con = PG.connect :dbname => configuration.params.database, :user => configuration.params.username
+          return if con
+        rescue PG::Error => e
+          raise PostgresException.new(e.message) and return
+        ensure
+          con.close if con
+        end
+      end
+    end
+  end
+end
